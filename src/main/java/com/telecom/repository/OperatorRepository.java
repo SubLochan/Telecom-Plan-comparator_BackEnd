c88f30732b4f3d7285ebf6ec1fb1962fe3d39af2
Lochan <sublarilochan123@gmail.com>
2026-03-15 11:43:19 +0530

    Delete src/main/java/com/telecom/repository directory
    patch 6

--- src/main/java/com/telecom/repository/OperatorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.telecom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.telecom.entity.Operator;
-
-public interface OperatorRepository extends JpaRepository<Operator,Long> {
-
-}
